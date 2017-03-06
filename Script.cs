1503e6c50bf7e6f1daba1e22ea109c98e9d7bad6
Loic4planet <loic.quattrociocchi@4planet.eu>
2017-03-06 22:49:37 +0100

    Init repository with script code

---
+++ Script.cs
@@ -0,0 +1,76 @@
+﻿        public Program() {
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed.
+}
+
+public void Save() {
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed.
+}
+
+public void Main(string argument) {
+    // The main entry point of the script, invoked every time
+    // one of the programmable block's Run actions are invoked.
+    // 
+    // The method itself is required, but the argument above
+    // can be removed if not needed.
+
+    /**
+     * Constants
+     */
+    const string nameLCD = "CC LCD ";
+    const string nameSonor = "CD-Block Sonor ";
+    const int nbLCD = 5;
+    const Color color = new Color(255, 0, 0, 255);
+    const bool alarm = true;
+
+    /**
+     * Variables
+     */
+    string time;
+    string timeTest;
+
+    // List Panels LCDs
+    List< IMyTextPanel> blocklist = new List< IMyTextPanel>();
+    // SoundBlock
+    IMySoundBlock soundWarning = GridTerminalSystem.GetBlockWithName( nameSonor + 1 ) as IMySoundBlock;
+
+    TimeSpan timeOffset = new TimeSpan(0, 0, 0, 0);     
+    DateTime currentTime = DateTime.Now;     
+    currentTime = currentTime.Add(timeOffset);
+
+    time = currentTime.ToString("HH:mm:ss");
+    timeTest = currentTime.ToString("HHmmss");
+
+    // Alarm clock, disable
+    if ( alarm == true && timeTest.CompareTo("020000") > 0 && timeTest.CompareTo("060000") < 0) {
+        // Play sound.
+        soundWarning.Play();
+        // Write alert in LCDs
+        time += "\nSleep Time!";
+    }   
+
+    // Write texts in Displays and show it.
+    IMyTextPanel display; 
+    for ( int i = 1; i <= nbLCD; i++ ) { 
+        display = GridTerminalSystem.GetBlockWithName( nameLCD + i ) as IMyTextPanel;
+
+        if ( alarm == true && timeTest.CompareTo("020000") > 0 && timeTest.CompareTo("060000") < 0) {
+            //display.SetValueFloat("FontColor", color ); 
+        }
+
+        // Write text in display
+        display.WritePublicText(time, false);
+
+        // Show public text in display
+        display.ShowPublicTextOnScreen(); 
+    }
+}
